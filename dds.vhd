471a3b7896a969922accaa2b8a21cdbfa24e05f7
Aaditya Talwai <talwai@dartmouth.edu>
2013-08-21 19:35:41 -0400

    dds update

--- dds.vhd
+++ dds.vhd
@@ -1,5 +1,3 @@
---Aaditya Talwai and Sam Golini
---VHDL model for Direct Digital Synthesis
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -8,30 +6,51 @@ use ieee.numeric_std.all;
 
 entity dds is
     port ( 
-          phase_in : in std_logic_vector(11 downto 0);
-        
-          clk : in std_logic;
+             phase_in : in std_logic_vector(11 downto 0);
 
-          dds_out : out std_logic_vector(11 downto 0)
-        );
+             clk : in std_logic;
+
+             dds_out : out std_logic_vector(11 downto 0)
+         );
 end dds;
 
 architecture behavior of dds is
-constant max_phase : integer := 2**12;
-signal count : std_logic_vector(11 downto 0) := "000000000000"
+    constant max_phase : integer := 2**12;
+    signal count : unsigned(11 downto 0) := "000000000000";
+    --signal LUT_in : std_logic_vector(11 downto 0);
+    constant fclk: integer:=25000000;
+    constant fsam: integer:=8000;
+    constant k: integer:=fclk/fsam;
+    signal clkdiv: integer;
+    signal samplerate: std_logic;
+    signal phase_in: std_logic_vector(11 downto 0);
 
 begin
+
+    clk_divider: process(clk)
+    begin
+        if rising_edge(clk) then -- on fast clock tick, increment clkdiv until it reaches divider value, then invert samplerate clock
+            if clkdiv = k-1 then
+                samplerate <= not(samplerate);
+                clkdiv <= 0;
+            else 
+                clkdiv <= clkdiv + 1;
+            end if;
+        end if;
+    end process clock_divider;
+
     phase_accumulator: process(phase_in, clk)
     begin
         if rising_edge(clk) then
-            	count <= count + phase_in;
+            if samplerate='1' then
+                count <= count + phase_in;
             end if;
         end if;
     end process phase_accumulator;
 
-    --TODO: lookup table interfacing with LUT Xilinx component
-    -- To convert from 2's complement (DDS output) to offset binary (DAC input)
-    -- invert MSB of DDS output
+    dds_out<=count;
+
+--TODO: lookup table interfacing with LUT Xilinx component
+-- To convert from 2's complement (DDS output) to offset binary (DAC input)
+-- invert MSB of DDS output
 end behavior;
-
-
